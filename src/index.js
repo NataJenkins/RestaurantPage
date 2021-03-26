f96184114b13a7af935fb73cf47926eb92378a70
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-03-26 09:24:33 -0600

    js navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import "./styless.css";
+// import "./styless.css";
+import navbar from "./navbar";
+
+const pageLoad = () => {
+  const htmlElements = [navbar()];
+  // const container = document.querySelector('#content');
+  // for (let j = 0; j <= htmlElements.length; j += 1) {
+  //   container.appendChild(htmlElements[j]);
+};
+pageLoad();
---
+++ src/navbar.js
@@ -0,0 +1,32 @@
+const navbar = (() => {
+  const contentContainer = document.querySelector("#content");
+
+  const nav = document.createElement("nav");
+  nav.innerHTML = `
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarNavAltMarkup"
+    aria-controls="navbarNavAltMarkup"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#"
+        >About <span class="sr-only">(current)</span></a
+      >
+      <a class="nav-item nav-link" href="#">Menu</a>
+      <a class="nav-item nav-link" href="#">Contact</a>
+    </div>
+  </div>
+</nav>`;
+  contentContainer.appendChild(nav);
+})();
+
+export default navbar;
