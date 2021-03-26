397d2dc0d9eb0399e752638f3153a6d0c74402a8
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-03-26 13:34:03 -0600

    Create about.js

---
+++ src/about.js
@@ -0,0 +1,23 @@
+const About = (() => {
+  const contentContainer = document.querySelector("#content");
+
+  const about = document.createElement("div");
+  about.innerHTML = `
+  <div class="about-container">
+  <h1>About Us</h1>
+  <div class="about-text">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+    placerat, tellus sit amet ultrices porttitor, ex ex aliquet purus,
+    pellentesque rhoncus felis ipsum nec purus. Donec id enim in risus
+    pellentesque blandit. Proin sed nibh nec arcu mollis rhoncus in sit
+    amet augue. Suspendisse magna nisl, eleifend non tempor et, luctus id
+    quam. Nulla bibendum vulputate ante, non malesuada eros lobortis
+    euismod
+  </div>
+</div>
+  `;
+  about.setAttribute("class", "js-about");
+  contentContainer.appendChild(about);
+})();
+
+export default About;
