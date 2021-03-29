0905c8a7bfef6eaea77458dd94467302d832b30a
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-03-29 17:20:43 -0600

    home page fix

--- src/home.js
+++ src/home.js
@@ -1,14 +1,14 @@
 const Home = (() => {
-  const contentContainer = document.querySelector('#content');
+  const contentContainer = document.querySelector("#content");
 
-  const header = document.createElement('header');
+  const header = document.createElement("header");
   header.innerHTML = `
   <div class="home-container tab-content active" id="home">
-  <img class="bg-img" src="assets/images/main.jpg" />
+  <img class="bg-img" src="../assets/images/main.jpg" />
   <h1 class="welcome">Welcome</h1>
   <h2>Greek Food and experience</h2>
 </div>`;
-  header.setAttribute('class', 'js-element');
+  header.setAttribute("class", "js-element");
   contentContainer.appendChild(header);
 })();
 
